e0ce9b5ac8f3c9d50751821ea7b0428b25457585
zhengping <zh771641@dal.ca>
2023-09-20 22:44:55 -0300

    some key help from chatGPT, still learning the VizDoom lib.

--- my_way_home/my_way_home_proj.py
+++ my_way_home/my_way_home_proj.py
@@ -1,6 +1,7 @@
 import vizdoom as vzd
 import itertools as it
 import random
+import matplotlib.pyplot as plt
 
 MAX_STUCK = 100
 
@@ -34,25 +35,41 @@ def distance(point1, point2):
 
 
 # Start Training:
+positions = []
+episodes = 1
 
-
-episodes = 100
 for i in range(episodes):
     game.new_episode()
+    stuck_counter = 0
+
+    prev_position = None
+
     while not game.is_episode_finished():
         state = game.get_state()
         action_idx = agent.choose_action(state)
         reward = game.make_action(actions[action_idx])
-        stuck_counter = 0
-        prev_position = None
-        while not game.is_episode_finished():
-            current_position = game.get_game_variable(vzd.GameVariable.POSITION_X), game.get_game_variable(
-                vzd.GameVariable.POSITION_Y)
-            if prev_position is not None and distance(current_position, prev_position) < 1:
-                stuck_counter += 1
-            else:
-                stuck_counter = 0
-            if stuck_counter > MAX_STUCK:
-                break
-            prev_position = current_position
-    print(f"Episode {i} finished with total reward: {game.get_total_reward()}")
+
+        current_position = game.get_game_variable(vzd.GameVariable.POSITION_X), game.get_game_variable(
+            vzd.GameVariable.POSITION_Y)
+
+        # For plot the map
+        positions.append(current_position)
+
+        if prev_position is not None and distance(current_position, prev_position) < 1:
+            stuck_counter += 1
+        else:
+            stuck_counter = 0
+        if stuck_counter > MAX_STUCK:
+            break
+        prev_position = current_position
+    print(f"Episode {i} total reward: {game.get_total_reward()}")
+
+    # print the road map
+    x_coords, y_coords = zip(*positions)
+    plt.figure(figsize=(10, 10))
+    plt.plot(x_coords, y_coords, '-o', markersize=2)
+    plt.title("Agent's Path")
+    plt.xlabel("X Coordinate")
+    plt.ylabel("Y Coordinate")
+    plt.grid(True)
+    plt.show()
